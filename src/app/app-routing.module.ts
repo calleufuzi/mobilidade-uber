e1a44f8e49c153f00bcadc14cb3191042be5aebb
Calleu  Fuzi <calleufuzi@Calleus-MacBook-Pro.local>
2019-06-30 01:01:08 -0300

    start of project uberhack

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/index', pathMatch: 'full' },
+  // { path: 'home', loadChildren: '../components/index/index.module#IndexModule' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
